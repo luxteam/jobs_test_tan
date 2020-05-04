1a9a5495c1f4d02d175e970f27483b9a9b0b4172
Ilia Nemankov <INemankov@luxoft.com>
2020-05-04 10:38:09 +0200

    Add base logic for run test without makeReport call

--- jobs/Scripts/simpleProcess.py
+++ jobs/Scripts/simpleProcess.py
@@ -19,70 +19,45 @@ import jobs_launcher.core.config as core_config
 from jobs_launcher.core.system_info import get_gpu
 from jobs_launcher.core.kill_process import kill_process
 
-ROOT_DIR = os.path.abspath(os.path.join(
-	os.path.dirname(__file__), os.path.pardir, os.path.pardir))
-PROCESS = ['TALibDopplerTest.exe', 'TALibTestConvolution.exe', 'TALibDopplerTest', 'TALibTestConvolution', 'talibdopplertest', 'talibtestconvolution']
-
 
 def createArgsParser():
 	parser = argparse.ArgumentParser()
 
-	parser.add_argument('--output', required=True, metavar="<dir>")
+	parser.add_argument('--output_path', required=True, metavar="<dir>")
+	parser.add_argument('--input_path', required=True, metavar="<dir>")
+	parser.add_argument('--response_path', required=True, metavar="<dir>")
 	parser.add_argument('--testType', required=True)
-	parser.add_argument('--res_path', required=True)
-	parser.add_argument('--pass_limit', required=True)
 	parser.add_argument('--testCases', required=True)
-	parser.add_argument('--error_count', required=False, default=0, type=int)
+	parser.add_argument('--timeout', required=False, default=120)
 
 	return parser
 
 
 def main(args):
+	args = createArgsParser().parse_args()
+
+	cases = []
 	try:
-		with open(os.path.join(os.path.dirname(__file__), 'base_functions.py')) as f:
-			script = f.read()
-	except OSError as e:
-		core_config.main_logger.error(str(e))
-		return 1
-
-	if os.path.exists(os.path.join(os.path.dirname(__file__), 'extensions', args.testType + '.py')):
-		with open(os.path.join(os.path.dirname(__file__), 'extensions', args.testType + '.py')) as f:
-			extension_script = f.read()
-		script = script.split('# place for extension functions')
-		script = script[0] + extension_script + script[1]
-
-	work_dir = os.path.abspath(args.output)
-	script = script.format(work_dir=work_dir, testType=args.testType, render_device=args.render_device, res_path=args.res_path, pass_limit=args.pass_limit,
-						   resolution_x=args.resolution_x, resolution_y=args.resolution_y, SPU=args.SPU, threshold=args.threshold)
-
-	with open(os.path.join(args.output, 'base_functions.py'), 'w') as file:
-		file.write(script)
-
-	if (os.path.exists(args.testCases) and '.json' in args.testCases):
-		with open(os.path.join(args.testCases)) as f:
-			tc = f.read()
-			test_cases = json.loads(tc)[args.testType]
-		necessary_cases = [
-			item for item in cases if item['case'] in test_cases]
-		cases = necessary_cases
+        with open(os.path.join(os.path.dirname(sys.argv[0]), args.testCases)) as f:
+            cases_list = json.load(f)
+        main_logger.info("Cases: {}".format([name['case'] for name in cases_list]))
+    except OSError as e:
+        main_logger.error("Failed to read test cases json. ")
+        main_logger.error(str(e))
+        exit(-1)
 
 	core_config.main_logger.info('Create empty report files')
-
-	if not os.path.exists(os.path.join(work_dir, 'Color')):
-		os.makedirs(os.path.join(work_dir, 'Color'))
-	copyfile(os.path.abspath(os.path.join(work_dir, '..', '..', '..', '..', 'jobs_launcher',
-										  'common', 'img', 'error.jpg')), os.path.join(work_dir, 'Color', 'failed.jpg'))
 
 	gpu = get_gpu()
 	if not gpu:
 		core_config.main_logger.error("Can't get gpu name")
-	render_platform = {platform.system(), gpu}
+	process_platform = {platform.system(), gpu}
 
 	for case in cases:
-		if sum([render_platform & set(skip_conf) == set(skip_conf) for skip_conf in case.get('skip_on', '')]):
+		if sum([process_platform & set(skip_conf) == set(skip_conf) for skip_conf in case.get('skip_on', '')]):
 			for i in case['skip_on']:
 				skip_on = set(i)
-				if render_platform.intersection(skip_on) == skip_on:
+				if process_platform.intersection(skip_on) == skip_on:
 					case['status'] = 'skipped'
 
 		if case['status'] != 'done':
@@ -92,12 +67,10 @@ def main(args):
 
 			template = core_config.RENDER_REPORT_BASE
 			template['test_case'] = case['case']
-			template['render_device'] = get_gpu()
+			template['process_device'] = get_gpu()
 			template['test_status'] = 'error'
 			template['script_info'] = case['script_info']
-			template['scene_name'] = case.get('scene', '')
-			template['file_name'] = 'failed.jpg'
-			template['render_color_path'] = os.path.join('Color', 'failed.jpg')
+			# TODO: add input files info
 			template['test_group'] = args.testType
 			template['date_time'] = datetime.now().strftime(
 				'%m/%d/%Y %H:%M:%S')
@@ -108,132 +81,62 @@ def main(args):
 	with open(os.path.join(work_dir, 'test_cases.json'), "w+") as f:
 		json.dump(cases, f, indent=4)
 
-	cmdRun = '"{tool}" -b -P "{template}"\n'.format(
-		tool=args.tool, template=os.path.join(args.output, 'base_functions.py'))
-
 	system_pl = platform.system()
-	if system_pl == "Windows":
-		cmdScriptPath = os.path.join(work_dir, 'script.bat')
-		with open(cmdScriptPath, 'w') as f:
-			f.write(cmdRun)
-	else:
-		cmdScriptPath = os.path.join(work_dir, 'script.sh')
-		with open(cmdScriptPath, 'w') as f:
-			f.write(cmdRun)
-		os.system('chmod +x {}'.format(cmdScriptPath))
-
-	core_config.main_logger.info('Launch script on Blender ({})'.format(cmdScriptPath))
-
-	p = subprocess.Popen(cmdScriptPath, shell=True,
-						 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	stdout, stderr = p.communicate()
-
-	with open(os.path.join(args.output, "renderTool.log"), 'a', encoding='utf-8') as file:
-		stdout = stdout.decode("utf-8")
-		file.write(stdout)
-
-	with open(os.path.join(args.output, "renderTool.log"), 'a', encoding='utf-8') as file:
-		file.write("\n ----STEDERR---- \n")
-		stderr = stderr.decode("utf-8")
-		file.write(stderr)
-
-	try:
-		rc = p.wait(timeout=100)
-	except psutil.TimeoutExpired as err:
-		rc = -1
-		for child in reversed(p.children(recursive=True)):
-			child.terminate()
-		p.terminate()
-
-	# TODO: check athena work in blender
-
-	return rc
-
-
-def group_failed(args):
-	try:
-		cases = json.load(open(os.path.realpath(
-			os.path.join(os.path.abspath(args.output), 'test_cases.json'))))
-	except Exception as e:
-		core_config.logging.error("Can't load test_cases.json")
-		core_config.main_logger.error(str(e))
-		exit(-1)
 
 	for case in cases:
-		if case['status'] == 'active':
-			case['status'] = 'skipped'
+		args_list = case["args_list"]
+		cmdRun = case["tool"]
 
-	with open(os.path.join(os.path.abspath(args.output), 'test_cases.json'), "w+") as f:
-		json.dump(cases, f, indent=4)
+		for arg in args_list.split(' '):
+			arg_type = case["args"]["arg"]["type"]
+			arg_value = arg_type = case["args"]["arg"]["value"]
+			if arg_type == "input_file":
+				arg_value = os.path.join(args.input_path, arg_value)
+			elif arg_type == "output_file":
+				arg_value = os.path.join(args.output_path, arg_value)
+			elif arg_type == "response_file":
+				arg_value = os.path.join(args.response_path, arg_value)
+			cmdRun = cmdRun + " " + arg_value
 
-	rc = main(args)
-	kill_process(PROCESS)
-	core_config.main_logger.info(
-		"Finish simpleRender with code: {}".format(rc))
-	exit(rc)
+		cmdRun = cmdRun + "\n"
+
+		if system_pl == "Windows":
+			cmdScriptPath = os.path.join(work_dir, 'script.bat')
+			with open(cmdScriptPath, 'w') as f:
+				f.write(cmdRun)
+		else:
+			cmdScriptPath = os.path.join(work_dir, 'script.sh')
+			with open(cmdScriptPath, 'w') as f:
+				f.write(cmdRun)
+			os.system('chmod +x {}'.format(cmdScriptPath))
+
+		core_config.main_logger.info('Launch script on {tool} ({path})'.format(tool=case["tool"], path=cmdScriptPath))
+
+		os.chdir(args.output)
+        p = psutil.Popen(cmdScriptPath, shell=True,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        rc = 1
+
+        try:
+            stdout, stderr = p.communicate(timeout=args.timeout)
+        except (psutil.TimeoutExpired, subprocess.TimeoutExpired) as err:
+            main_logger.error("Process has been aborted by timeout")
+            rc = -1
+            for child in reversed(p.children(recursive=True)):
+                child.terminate()
+            p.terminate()
+        finally:
+
+            with open("process_log.txt", 'a', encoding='utf-8') as file:
+                stdout = stdout.decode("utf-8")
+                file.write(stdout)
+
+            with open("process_log.txt", 'a', encoding='utf-8') as file:
+                file.write("\n ----STEDERR---- \n")
+                stderr = stderr.decode("utf-8")
+                file.write(stderr)
 
 
 if __name__ == "__main__":
-	core_config.main_logger.info("simpleRender start working...")
-	
-	args = createArgsParser().parse_args()
-
-	iteration = 0
-
-	try:
-		os.makedirs(args.output)
-	except OSError as e:
-		pass
-	
-	try:
-		copyfile(os.path.realpath(os.path.join(os.path.dirname(
-					__file__), '..', 'Tests', args.testType, 'test_cases.json')),
-				os.path.realpath(os.path.join(os.path.abspath(
-					args.output), 'test_cases.json')))
-	except:
-		core_config.logging.error("Can't copy test_cases.json")
-		core_config.main_logger.error(str(e))
-		exit(-1)
-
-	while True:
-		iteration += 1
-
-		core_config.main_logger.info(
-			'Try to run script in blender (#' + str(iteration) + ')')
-
-		rc = main(args)
-
-		try:
-			move(os.path.join(os.path.abspath(args.output), 'renderTool.log'),
-				 os.path.join(os.path.abspath(args.output), 'renderTool' + str(iteration) + '.log'))
-		except:
-			core_config.main_logger.error('No renderTool.log')
-
-		try:
-			cases = json.load(open(os.path.realpath(
-				os.path.join(os.path.abspath(args.output), 'test_cases.json'))))
-		except Exception as e:
-			core_config.logging.error("Can't load test_cases.json")
-			core_config.main_logger.error(str(e))
-			exit(-1)
-
-		active_cases = 0
-		current_error_count = 0
-
-		for case in cases:
-			if case['status'] in ['fail', 'error', 'inprogress']:
-				current_error_count += 1
-				if args.error_count == current_error_count:
-					group_failed(args)
-			else:
-				current_error_count = 0
-
-			if case['status'] in ['active', 'fail', 'inprogress']:
-				active_cases += 1
-
-		if active_cases == 0 or iteration > len(cases) * 2:	# 2- retries count		
-			# exit script if base_functions don't change number of active cases
-			kill_process(PROCESS)
-			core_config.main_logger.info(
-				"Finish simpleRender with code: {}".format(rc))
-			exit(rc)
+	core_config.main_logger.info("simpleProcess start working...")
+	exit(main())
