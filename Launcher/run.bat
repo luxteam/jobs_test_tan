7678b720e1b6d7eac88944852135d66875de125f
Piromancer <Den2000_09@mail.ru>
2020-05-25 15:06:24 +0300

    Added parametrized run.bat

--- Launcher/run.bat
+++ Launcher/run.bat
@@ -3,4 +3,5 @@ set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 python3 -m pip install --upgrade pip wheel setuptools
 python3 -m pip install -r requirements.txt
 
-pytest ../Autotests/Convolution/Convolution.py --alluredir=../allure-results
+pytest --alluredir=../allure-results %1
+REM allure generate -c ../allure-results -o ../Report
