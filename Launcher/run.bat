a468bbad64772805b667ab117da1bd647d1ed193
Piromancer <44932328+Piromancer@users.noreply.github.com>
2020-05-26 19:37:25 +0300

    Added upgrade flag to pip install

--- Launcher/run.bat
+++ Launcher/run.bat
@@ -1,7 +1,7 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 
 python -m pip install --upgrade pip wheel setuptools
-python -m pip install -r requirements.txt
+python -m pip install -r --upgrade requirements.txt
 
 if %1 == FULL (pytest --alluredir=../allure-results ../Autotests/Convolution/ ../Autotests/Doppler/) else (pytest --alluredir=../allure-results ../Autotests/%1/)
 REM allure generate -c ../allure-results -o ../Report
