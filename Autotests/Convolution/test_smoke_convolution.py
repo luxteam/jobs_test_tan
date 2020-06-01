31a6eb085a0edfe35f7d27d5481677b592e65be1
Piromancer <Den2000_09@mail.ru>
2020-06-01 19:01:31 +0300

    Temporary solved path issue

--- Autotests/Convolution/test_smoke_convolution.py
+++ Autotests/Convolution/test_smoke_convolution.py
@@ -25,19 +25,19 @@ class TestSmoke:
     @allure.issue('https://adc.luxoft.com/jira/browse/STVITT-53', 'GPU-OV causes error')
     @pytest.mark.xfail(condition=lambda: True, reason='Error after outputing file')
     def test_conv_001(self):
-        runConvolution("GPU-OV", "smokeIn.wav", "conv_001.wav", "testresponse.wav", "conv_001.wav")
+        runConvolution("GPU-OV", "smokeIn.wav", "conv_001.wav", "../IRs/testresponse.wav", "conv_001.wav")
 
     @allure.title("CONV_SM_002")
     @allure.description("""Uniform Partitioned GPU""")
     @pytest.mark.timeout(150)
     def test_conv_002(self):
-        runConvolution("GPU-UN", "smokeIn.wav", "conv_002.wav", "testresponse.wav", "conv_001.wav")
+        runConvolution("GPU-UN", "smokeIn.wav", "conv_002.wav", "../IRs/testresponse.wav", "conv_001.wav")
 
     @allure.title("CONV_SM_003")
     @allure.description("""Non-uniform partitioned GPU""")
     @pytest.mark.timeout(150)
     def test_conv_003(self):
-        runConvolution("GPU-NU", "smokeIn.wav", "conv_003.wav", "testresponse.wav", "conv_001.wav")
+        runConvolution("GPU-NU", "smokeIn.wav", "conv_003.wav", "../IRs/testresponse.wav", "conv_001.wav")
 
     @allure.title("CONV_SM_004")
     @allure.description("""Overlap Add CPU""")
@@ -45,7 +45,7 @@ class TestSmoke:
     @allure.issue('https://adc.luxoft.com/jira/browse/STVITT-54', 'CPU modes don\'t work')
     @pytest.mark.xfail(condition=lambda: True, reason='CPU mode does not work')
     def test_conv_004(self):
-        runConvolution("CPU-OV", "smokeIn.wav", "conv_004.wav", "testresponse.wav", "conv_001.wav")
+        runConvolution("CPU-OV", "smokeIn.wav", "conv_004.wav", "../IRs/testresponse.wav", "conv_001.wav")
 
     @allure.title("CONV_SM_005")
     @allure.description("""Uniform partitioned CPU""")
@@ -53,7 +53,7 @@ class TestSmoke:
     @allure.issue('https://adc.luxoft.com/jira/browse/STVITT-54', 'CPU modes don\'t work')
     @pytest.mark.xfail(condition=lambda: True, reason='CPU mode does not work')
     def test_conv_005(self):
-        runConvolution("CPU-UN", "smokeIn.wav", "conv_005.wav", "testresponse.wav", "conv_001.wav")
+        runConvolution("CPU-UN", "smokeIn.wav", "conv_005.wav", "../IRs/testresponse.wav", "conv_001.wav")
 
     @allure.title("CONV_SM_006")
     @allure.description("""Non-uniform partitioned CPU""")
@@ -61,4 +61,4 @@ class TestSmoke:
     @allure.issue('https://adc.luxoft.com/jira/browse/STVITT-54', 'CPU modes don\'t work')
     @pytest.mark.xfail(condition=lambda: True, reason='CPU mode does not work')
     def test_conv_006(self):
-        runConvolution("CPU-NU", "smokeIn.wav", "conv_006.wav", "testresponse.wav", "conv_001.wav")
+        runConvolution("CPU-NU", "smokeIn.wav", "conv_006.wav", "../IRs/testresponse.wav", "conv_001.wav")
